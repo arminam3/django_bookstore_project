a65e4ce2573e216470fbf686dd0d268992cfc1c3
unknown <armin.tree@gmail.com>
2022-05-11 23:42:18 +0430

    add media to project

--- books/models.py
+++ books/models.py
@@ -6,7 +6,8 @@ class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=1000)
     description = models.TextField()
-    price = models.DecimalField(max_digits=5,decimal_places=2)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    cover = models.ImageField(upload_to='book_covers', null=True, blank=True)
 
     def get_absolute_url(self):
         return reverse('book_details', args=[self.id])
